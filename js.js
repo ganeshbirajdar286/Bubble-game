2436df021169bfc4af8f2ee8aaa06c98046b4450
ganeshbirajdar <ganeshbirajdar286@gmail.com>
2024-12-06 22:42:07 +0530

    add

---
+++ js.js
@@ -0,0 +1,46 @@
+
+
+let bubblemaker=()=>{
+    let clutter="";
+for(let i=1;i<=120;i++){
+clutter +=`<div class="bubble">${Math.floor(Math.random()*10)}</div>`;
+
+}
+document.querySelector("#btpanel").innerHTML=clutter;
+}
+bubblemaker();
+
+
+let timer=60;
+let runTimer=()=>{
+let interval= setInterval(()=>{
+   if(timer>0){ timer--;
+    document.querySelector("#timerval").innerText=timer;}
+    else{
+        clearInterval(interval);
+        document.querySelector("#btpanel").innerHTML= "<h1>Game over</h1>";
+    }
+},1000);
+}
+runTimer();
+let hitrn=0;
+let newhit=()=>{
+hitrn=  Math.floor(Math.random()*10);
+document.querySelector("#hitval").innerText=hitrn;
+}
+newhit();
+let score=0;
+let  newscore=()=>{
+score+=10;
+document.querySelector("#scoreval").innerText=score;
+}
+
+
+document.querySelector("#btpanel").addEventListener("click",(e)=>{
+let clickno=(Number(e.target.innerText));
+if(hitrn===clickno){
+newscore();
+bubblemaker();
+newhit();
+}
+}) 
